d5aa224b9f5a2473fbfdfa5bbc171e8b2681f9a6
Carl <carlbradshaw.is@gmail.com>
2013-01-30 20:58:27 +1300

    map page
    also edited the line gratient and footer

--- js/main.js
+++ js/main.js
@@ -20,4 +20,52 @@ $("#homeImgs").backstretch(["./img/home_img/image_1.jpg", "./img/home_img/image_
 		duration : 50000
 	});
 
+//==================== MAP GUFF ========================//
+
+	$('.map-hov').hover(function(){
+		$(this).css({opacity:1});
+	},function(){
+		$('.map-hov').not('.mapsel').css({opacity:0});
+	});
+
+	$('.map-hov').click(function(e){
+		e.preventDefault();
+
+
+		$('.map-hov').removeClass('mapsel');
+		$(this).addClass('mapsel');
+
+		
+		$('.map-hov').css({opacity:0});
+		$(this).css({opacity:1});
+
+
+
+		$('.sound').remove();
+		$('#dummy').append(' <embed class="sound" src="Pop.aiff" hidden="true" autostart="true" loop="false" />');
+		// setTimeout(function(){
+  // 			$('.sound').remove();
+		// }, 2000);
+
+	var region = $(this).attr('title') // REGION TITLE VAR
+
+	$('.box3').animate({opacity:0},
+		function(){
+		$('.top5 span').html(region); // DISPLAY REGION TITLE
+		});
+
+	if($('.open').length < 1){
+	$('.box2').show().animate({width:"100%",height:'100%'},100);
+	}
+
+
+	$('.box3').animate({opacity:1});
+
+	$('.box').addClass('open');
+
+	});//.click
+
+
+//==================== MAP GUFF END =====================//
+
 });//end ready handler
